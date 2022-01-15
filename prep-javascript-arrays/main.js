dbe26ec03d956dbdf07007a8dc350fb7599a071a
Rosemary Liu <rliu18@gmail.com>
2022-01-15 14:19:59 -0800

    2nd Commit. Edited last array element.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -10,5 +10,5 @@ console.log('the 3rd element of the array is', thirdElement);
 var length = languages.length;
 console.log('the length of the array is', length);
 
-var lastElement = languages[5];
+var lastElement = languages[languages.length - 1];
 console.log('the last element of the array is', lastElement);
